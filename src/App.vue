0c08bdc60e8d02105f430ccdff755c9f2ae91566
zalbamonteiro <rosalbasmonteiro@gmail.com>
2021-07-29 22:10:12 -0300

    adiona header

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,113 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import Header from "./components/Header.vue";
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  export default {
+    name: 'App',
+    components : {
+      Header
+    }
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  html, body {
+    margin: 0px;
+    padding: 0px;
+    font-family: sans-serif
+  }
+
+  .container {
+    width: 100%;
+    max-width: 960px;
+    display: flex;
+    margin: auto;
+    flex-wrap: wrap;
+  } 
+
+  .row { 
+    display: flex;
+    flex-direction: row;
+    flex-wrap: nowrap;
+    align-items: center;
+    flex: 1;
+    width: 100%;
+  }
+
+  .col-md-1{
+      width: calc(960px/12 * 1);
+      box-sizing: border-box;
+      padding: 0px 10px
+  }
+
+  .col-md-2{
+      width: calc(960px/12 * 2);
+      box-sizing: border-box;
+      padding: 0px 10px
+  }
+
+  .col-md-3{
+      width: calc(960px/12 * 3);
+      box-sizing: border-box;
+      padding: 0px 10px
+  }
+
+  .col-md-4{
+      width: calc(960px/12 * 4);
+      box-sizing: border-box;
+      padding: 0px 10px
+  }
+
+  .col-md-5{
+      width: calc(960px/12 * 5);
+      box-sizing: border-box;
+      padding: 0px 10px
+  }
+
+  .col-md-6{
+      width: calc(960px/12 * 6);
+      box-sizing: border-box;
+      padding: 0px 10px
+  }
+
+  .col-md-7{
+      width: calc(960px/12 * 7);
+      box-sizing: border-box;
+      padding: 0px 10px
+  }
+
+  .col-md-8{
+      width: calc(960px/12 * 8);
+      box-sizing: border-box;
+      padding: 0px 10px
+  }
+
+  .col-md-9{
+      width: calc(960px/12 * 9);
+      box-sizing: border-box;
+      padding: 0px 10px
+  }
+
+  .col-md-10{
+      width: calc(960px/12 * 10);
+      box-sizing: border-box;
+      padding: 0px 10px
+  }
+
+  .col-md-11{
+      width: calc(960px/12 * 11);
+      box-sizing: border-box;
+      padding: 0px 10px
+  }
+
+  .col-md-12{
+      width: calc(960px/12 * 12);
+      box-sizing: border-box;
+      padding: 0px 10px
+  }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,95 @@
+<template>
+  <header class="header">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6">
+          <h1 class="logo">
+            <img class="logo__img" src="../assets/logo.png" />
+          </h1>
+        </div>
+        <div class="col-md-6 header__right">
+          <p class="header__text">Apoido por mais 50 mil manos</p>
+          <nav class="menu">
+            <ul>
+              <li>Home</li>
+              <li>Biografia</li>
+              <li>Notícias</li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+    </div>
+  </header>
+</template>
+<script>
+  export default {
+    name : "Header"
+  }
+</script>
+<style>
+  .header {
+    background-color : #000;
+    height : 80px;
+    width : 100%;
+    color: white;
+  }
+
+  .header__right{
+    display: flex;
+    flex-direction: column;
+    align-items: flex-end;
+  }
+
+  .logo{
+    width: 160px;
+    padding: 0px;
+    margin-top: 15px;
+  }
+
+  .logo__img{
+    width: 100%;
+  }
+
+  .header__text {
+    color: #e2e2e2;
+    font-size: 14px;
+    font-style: italic;
+    margin: 8px;
+    font-weight: 700;
+  }
+
+  .menu ul{
+    list-style: none;
+    padding: 0;
+    margin : 0;
+    color: #e2e2e2;
+    display: flex;
+    flex-direction: row;
+    width: 100%;
+    align-items: center;
+  }
+
+  .menu ul li{
+    padding: 4px 14px;
+    font-size: 16px;
+    text-transform: uppercase;
+    font-weight: 900;
+    letter-spacing: 0.5px;
+  }
+
+  .menu ul li:not(:last-child):after{
+  content: "";
+  display: inline-block;
+  width: 1px;
+  background-color: #e2e2e2;
+  height: 10px;
+  margin-left: 10px;
+}
+
+.menu ul li:hover {
+  background-color: #333;
+}
+
+
+
+</style>
