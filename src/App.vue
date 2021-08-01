145802e999a416808761b9fc455d79812417430d
zalbamonteiro <rosalbasmonteiro@gmail.com>
2021-08-01 20:49:28 -0300

    Aula 7 - Componentes parte 2 (data e métodos)

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,21 @@
 <template>
   <div id="app">
     <Header />
+    <main>
+      <Banner />
+    </main>
   </div>
 </template>
 
 <script>
   import Header from "./components/Header.vue";
+  import Banner from "./components/Banner.vue";
 
   export default {
     name: 'App',
     components : {
-      Header
+      Header,
+      Banner
     }
   }
 </script>
